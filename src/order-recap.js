ae022b0171dcc4ebbd37bbb15be05672863fb09e
Kevin Cabon <cabonk.pro@pm.me>
2020-05-25 19:14:32 +0200

    add table witch infos to order-recap

--- src/order-recap.js
+++ src/order-recap.js
@@ -1,16 +1,21 @@
 import { loadHeaderFooter } from './include/loadHeaderFooter';
 
-let price = 0;
+let price = parseFloat(0);
 
 loadHeaderFooter();
 
 if (localStorage.orderRecap){
     let content = JSON.parse(localStorage.orderRecap);
+    console.log(content);
     for (var i in content.products){
         price += content.products[i].price;
     }
+    document.getElementById('userName').innerHTML = content.contact.firstName + ",";
+    document.getElementById('order-email').innerHTML = content.contact.email;
     document.getElementById('order-id').innerHTML = content.orderId;
-    document.getElementById('order-total-price').innerHTML = price;
+    document.getElementById('order-userName').innerHTML = content.contact.lastName + " " + content.contact.firstName;
+    document.getElementById('order-address').innerHTML = content.contact.address + ", " + content.contact.city;
+    document.getElementById('order-total-price').innerHTML = (price / 100).toFixed(2) + "€";
     delete localStorage.orderList;
 }else{
     window.location.href = 'order.html';
